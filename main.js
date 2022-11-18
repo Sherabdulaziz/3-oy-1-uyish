6c24eb43268fd5e31b53cb1c0563ee7ae7b23a10
Sherabdulaziz <greatboy398@gmail.com>
2022-11-18 21:28:28 +0500

    yangi oy

---
+++ main.js
@@ -0,0 +1,16 @@
+let a = alert("assalomu alaykum Xush kelibsiz");
+let firstname = prompt("ismingizni kiriting");
+let lastname = prompt("familyangizni kiriting");
+let age = +prompt("yoshingizni kiriting");
+let interesting = prompt("qiziqishlaringizni kiriting");
+let job = prompt("kasb kiriting");
+let friends = prompt("do'stlaringizni kiriting");
+const person = [
+  (ismi = firstname),
+  (familyasi = lastname),
+  (yoshi = age),
+  (qiziqishi = interesting),
+  (kasbi = job),
+  (dusti = friends),
+];
+console.log(person);
